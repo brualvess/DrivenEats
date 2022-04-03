21e3723ee35d00eec769cc3a576d37a6debeea41
brualvess <oliveirabrunna2210@gmail.com>
2022-04-02 22:51:02 -0300

    seleção de pedido

--- scripit.js
+++ scripit.js
@@ -1,4 +1,31 @@
-function escolher(elemento) {
+function escolherBurguer(elemento) {
+  const burguer = document.getElementById("burguer");
+  const hamburguer = document.getElementById("hamburguer");
+  const burgao = document.getElementById("burgao");
+  burguer.classList.remove("selecionado");
+  hamburguer.classList.remove("selecionado");
+  burgao.classList.remove("selecionado");
   elemento.classList.toggle("selecionado");
-  
+
 }
+function escolherBebida(elemento) {
+  const coca = document.getElementById("coca");
+  const guarana = document.getElementById("guarana");
+  const fanta = document.getElementById("fanta");
+  coca.classList.remove("selecionado");
+  guarana.classList.remove("selecionado");
+  fanta.classList.remove("selecionado");
+  elemento.classList.toggle("selecionado");
+}
+function escolherSobremesa(elemento) {
+  const cake = document.getElementById("cake");
+  const pudim = document.getElementById("pudim");
+  const sundae = document.getElementById("sundae");
+  cake.classList.remove("selecionado");
+  pudim.classList.remove("selecionado");
+  sundae.classList.remove("selecionado");
+  elemento.classList.toggle("selecionado");
+
+}
+
+

b0e46cd77002ff538cdd7578cad58d6714481c80
brualvess <oliveirabrunna2210@gmail.com>
2022-04-03 21:54:00 -0300

    botão finalizar pedido

--- scripit.js
+++ scripit.js
@@ -6,7 +6,7 @@ function escolherBurguer(elemento) {
   hamburguer.classList.remove("selecionado");
   burgao.classList.remove("selecionado");
   elemento.classList.toggle("selecionado");
-
+  verificarItens();
 }
 function escolherBebida(elemento) {
   const coca = document.getElementById("coca");
@@ -16,6 +16,7 @@ function escolherBebida(elemento) {
   guarana.classList.remove("selecionado");
   fanta.classList.remove("selecionado");
   elemento.classList.toggle("selecionado");
+  verificarItens();
 }
 function escolherSobremesa(elemento) {
   const cake = document.getElementById("cake");
@@ -25,7 +26,18 @@ function escolherSobremesa(elemento) {
   pudim.classList.remove("selecionado");
   sundae.classList.remove("selecionado");
   elemento.classList.toggle("selecionado");
-
+  verificarItens();
 }
-
-
+function verificarItens() {
+  let quantidade = document.querySelectorAll(".selecionado").length;
+  if (quantidade == 3) {
+    const elemento = document.querySelector(".finalizarPedido");
+    elemento.innerText = "Fechar pedido";
+    elemento.classList.add("fecharPedido");
+    const botaoVerde = document.querySelector(".finalizar");
+    botaoVerde.classList.add("finish");
+  }
+}
+function pedirPedido() {
+  console.log("amor")
+}
